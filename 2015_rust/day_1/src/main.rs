19d3ded68efed1092acf6a71b0c93d95723a1edc
octtoberrust <octtoberrust@outlook.com>
2024-11-23 09:48:43 -0600

    day 1 solved

--- 2015_rust/day_1/src/main.rs
+++ 2015_rust/day_1/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let mut contents = fs::read_to_string("advent_of_code_day_1_text_input.txt")?;
+    
+    let mut counter = 0;
+    
+    for character in contents.chars() {
+        match character {
+            '(' => counter += 1,
+            ')' => counter -= 1,
+            _ => (),
+        }
+    }
+
+    println!("counter: {counter}");
+    Ok(())
 }
