21a3e03dc994f73bb75ef50053c803a3c7e7ecf1
octtoberrust <octtoberrust@outlook.com>
2024-11-26 20:53:19 -0600

    day 3, part 1 solved

--- 2015_rust/day_3/src/main.rs
+++ 2015_rust/day_3/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::collections::HashSet;
+
+fn main() -> std::io::Result<()> {
+    let mut contents = fs::read_to_string("santa_directions.txt")?;
+    let mut unique_houses = HashSet::new();
+    let mut x = 0;
+    let mut y = 0;
+
+    unique_houses.insert((x, y));
+
+    for character in contents.chars() {
+        println!("{character}");
+
+        match character {
+            '^' => x += 1,
+            'v' => x -= 1,
+            '<' => y -= 1,
+            '>' => y += 1,
+            _ => (),
+        }
+
+        unique_houses.insert((x, y));
+    }
+
+    println!("{:?}", unique_houses);
+    println!("{}", unique_houses.len());
+
+    Ok(())
 }
