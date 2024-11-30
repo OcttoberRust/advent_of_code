668605ab1f25e05c457ec502ed78faed06a74625
octtoberrust <octtoberrust@outlook.com>
2024-11-30 09:03:20 -0600

    optimizing day 3, part 2

--- 2015_rust/day_3/src/main.rs
+++ 2015_rust/day_3/src/main.rs
@@ -1,31 +1,26 @@
 use std::fs;
-use std::str::Chars;
 use std::collections::HashSet;
 
 fn main() -> std::io::Result<()> {
-    let mut contents = fs::read_to_string("santa_directions.txt")?;
+    let contents = fs::read_to_string("santa_directions.txt")?;
     let mut unique_houses: HashSet<(i32, i32)> = HashSet::new();
     let mut santa_coords = Coordinates { x: 0, y: 0 };
     let mut robo_santa_coords = Coordinates { x: 0, y: 0 };
-    let mut iteration = 0;
-    let (mut x, mut y) = (0, 0); 
 
     unique_houses.insert((0, 0));
 
-    for character in contents.chars() {
-
-        if(iteration % 2 == 0) {
-            (x, y) =  track_coords(character, &mut santa_coords);
-            unique_houses.insert((x, y));
+    let directions: Vec<char> = contents.chars().collect();
+    for (i, &direction) in directions.iter().enumerate() {
+        if i % 2 == 0 {
+            santa_coords.update(direction);
+            unique_houses.insert((santa_coords.x, santa_coords.y));
         } else {
-            (x, y) = track_coords(character, &mut robo_santa_coords);
-            unique_houses.insert((x, y));
+            robo_santa_coords.update(direction);
+            unique_houses.insert((robo_santa_coords.x, robo_santa_coords.y));
         }
-        
-        iteration += 1;
     }
 
-    println!("{}", unique_houses.len());
+    println!("Unique houses visited: {}", unique_houses.len());
 
     Ok(())
 }
@@ -35,15 +30,14 @@ struct Coordinates {
     y: i32,
 }
 
-fn track_coords(character: char, coords: &mut Coordinates) -> (i32, i32) {
-    match character {
-        '^' => coords.x += 1,
-        'v' => coords.x -= 1,
-        '<' => coords.y -= 1,
-        '>' => coords.y += 1,
-        _ => (),
+impl Coordinates {
+    fn update(&mut self, direction: char) {
+        match direction {
+            '^' => self.y += 1,
+            'v' => self.y -= 1,
+            '<' => self.x -= 1,
+            '>' => self.x += 1,
+            _ => (),
+        }
     }
-
-    (coords.x, coords.y) 
 }
-
