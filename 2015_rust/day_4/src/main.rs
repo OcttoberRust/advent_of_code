77a8cea5549f15034a2e6b61d466ce511ecef8dc
octtoberrust <octtoberrust@outlook.com>
2024-12-01 14:38:57 -0600

    2015 day 4, part 1 and 2 solved

--- 2015_rust/day_4/src/main.rs
+++ 2015_rust/day_4/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::thread;
+use std::sync::Arc;
+use md5;
+
+fn main() -> std::io::Result<()> {
+    let contents = Arc::new(fs::read_to_string("2015_day_4_input.txt")?.trim().to_string());
+
+    let mut handles = vec![];
+
+    let ranges = vec![
+        (1,        1000000),
+        (1000001,   2000000),
+        (2000001,   3000000),
+        (3000001,   4000000),
+        (4000001,   5000000),
+        (5000001,   6000000),
+        (6000001,   7000000),
+        (7000001,   8000000),
+    ];
+
+    println!("Starting computation...");
+
+    for (start, end) in ranges {
+        let data_clone = Arc::clone(&contents);
+        let handle = thread::spawn(move || {
+            for i in start..=end {
+                let input = format!("{}{}", data_clone, i);
+
+                let digest = md5::compute(input.as_bytes());
+                let hash_str = format!("{:x}", digest);
+
+                if hash_str.starts_with("000000") {
+                    println!("i = {}", i);
+                    println!("The matching hash is: {}", hash_str);
+                    break;
+                }
+            }
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Computation completed.");
+
+    Ok(())
 }
