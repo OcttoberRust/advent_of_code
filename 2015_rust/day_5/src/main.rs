45467457f4a0059baf69615d7db7cc773eff92ee
octtoberrust <octtoberrust@outlook.com>
2024-12-02 07:48:33 -0600

    read input file

--- 2015_rust/day_5/src/main.rs
+++ 2015_rust/day_5/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("2015_day_5_input.txt");
+
+    for line in contents {
+        println!("{line}");
+    }
 }
