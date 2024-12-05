eb46c7267d22084cdae64fac6226d3be027a12a5
octtoberrust <octtoberrust@outlook.com>
2024-12-04 19:22:13 -0600

    day 5, part 1 refined

--- 2015_rust/day_5/src/main.rs
+++ 2015_rust/day_5/src/main.rs
@@ -1,88 +1,33 @@
 use std::fs;
-use regex::Regex;
+use itertools::Itertools;
 
 fn main() -> std::io::Result<()> {
     let contents = fs::read_to_string("2015_day_5_input.txt")?;
     let mut nice_string_counter = 0;
 
     for line in contents.lines() {
-        println!("{line}");
-        
-        let mut criteria_met = 0;
-
-        if has_three_vowels(&line) {
-            criteria_met += 1;
-        }
-
-        println!("Criteria met: {}", criteria_met);
-        
-        if has_double_letter(&line) {
-            criteria_met += 1;
-        }
-
-        println!("Criteria met: {}", criteria_met);
-
-        if does_not_have_defined_str(&line) {
-            criteria_met += 1;
-        }
-
-        println!("Criteria met: {}", criteria_met);
-        
-        if criteria_met == 3 {
-            nice_string_counter += 1;
-        }
+        if has_three_vowels(&line)
+            && has_double_letter(&line)
+            && does_not_have_defined_str(&line)
+            {
+                nice_string_counter += 1;
+            }
     }
 
-    println!("# of nice strings: {nice_string_counter}");
+    println!("# of nice strings: {}", nice_string_counter);
 
     Ok(())
 }
 
 fn has_three_vowels(line: &str) -> bool {
-
-    let mut vowel_counter = 0;
-    
-    for char in line.chars() {
-        println!("{char} ");
-        match char {
-            'a' => vowel_counter += 1,
-            'e' => vowel_counter += 1,
-            'i' => vowel_counter += 1,
-            'o' => vowel_counter += 1,
-            'u' => vowel_counter += 1,
-             _  => (),
-        }
-
-        if vowel_counter == 3 {
-            return true
-        }
-    }
-    
-    false
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    line.chars().filter(|c| vowels.contains(c)).take(3).count() == 3
 }
 
 fn has_double_letter(line: &str) -> bool {
-
-    let mut chars = line.chars().peekable();
-
-    while let Some(current_char) = chars.next() {
-        if let Some(&next_char) = chars.peek() {
-            if current_char == next_char {
-                return true;
-            }
-        }
-    }
-
-    false
+    line.chars().tuple_windows().any(|(a, b)| a == b)
 }
 
 fn does_not_have_defined_str(line: &str) -> bool {
-
-    let re = Regex::new(r"(ab|cd|pq|xy)").unwrap();
-
-    if re.is_match(line) {
-        return false;
-    }
-
-    true
+    !line.contains("ab") && !line.contains("cd") && !line.contains("pq") && !line.contains("xy")
 }
