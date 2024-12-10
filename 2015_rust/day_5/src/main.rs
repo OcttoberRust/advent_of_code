4a128706ca4db689bd95c29296088b5ae58eabca
octtoberrust <octtoberrust@outlook.com>
2024-12-09 19:20:07 -0600

    day 5, part 2 update

--- 2015_rust/day_5/src/main.rs
+++ 2015_rust/day_5/src/main.rs
@@ -1,14 +1,22 @@
 use std::fs;
-use itertools::Itertools;
+use std::collections::HashMap;
 
 fn main() -> std::io::Result<()> {
     let contents = fs::read_to_string("2015_day_5_input.txt")?;
     let mut nice_string_counter = 0;
 
     for line in contents.lines() {
+        /*
         if has_three_vowels(&line)
             && has_double_letter(&line)
             && does_not_have_defined_str(&line)
+            {
+                nice_string_counter += 1;
+            }
+            */
+
+        if has_two_nonoverlapping_pairs(&line)
+            && has_one_letter_in_between(&line)
             {
                 nice_string_counter += 1;
             }
@@ -19,6 +27,7 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
+/*
 fn has_three_vowels(line: &str) -> bool {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     line.chars().filter(|c| vowels.contains(c)).take(3).count() == 3
@@ -30,4 +39,25 @@ fn has_double_letter(line: &str) -> bool {
 
 fn does_not_have_defined_str(line: &str) -> bool {
     !line.contains("ab") && !line.contains("cd") && !line.contains("pq") && !line.contains("xy")
+}
+
+*/
+
+fn has_two_nonoverlapping_pairs(line: &str) -> bool {
+
+    let char_a = line.chars();
+    let char_b = line.chars().skip(1);
+    let mut map: HashMap<usize, (char, char)> = HashMap::new();
+
+    for (i, (a, b)) in char_a.zip(char_b).enumerate() {
+        println!("pair: ({}, {})", a, b);
+        map.insert(i, (a, b));
+    }
+
+    true
+}
+
+fn has_one_letter_in_between(line: &str) -> bool {
+    
+    true
 }
