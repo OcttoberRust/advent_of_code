b62cb625502baaad788bcdcb1082b12490cc9453
octtoberrust <octtoberrust@outlook.com>
2025-01-13 20:06:39 -0600

    small update to read input file on day 7

--- 2015_rust/day_7/src/main.rs
+++ 2015_rust/day_7/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let contents = fs::read_to_string("2015_day_7_input.txt")?;
+
+    for line in contents.lines() {
+        println!("{line}");
+    }
+
+    Ok(())
 }
