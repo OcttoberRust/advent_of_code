3ae23383c79d04faa26da139807e8206535d6272
octtoberrust <octtoberrust@outlook.com>
2025-01-14 21:47:13 -0600

    small update with fn to implement later

--- 2015_rust/day_7/src/main.rs
+++ 2015_rust/day_7/src/main.rs
@@ -1,12 +1,27 @@
 use std::fs;
 use std::io;
 
+enum Instruction {
+    //need to implement
+}
+
 fn main() -> io::Result<()> {
     let contents = fs::read_to_string("2015_day_7_input.txt")?;
 
     for line in contents.lines() {
         println!("{line}");
+        if line.trim().is_empty() {
+            continue;
+        }
     }
 
     Ok(())
+}
+
+fn parse_line(line: &str) {
+
+}
+
+fn get_signal() {
+
 }
